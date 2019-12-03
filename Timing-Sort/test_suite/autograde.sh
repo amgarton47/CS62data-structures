c36add77204433b9c63f00d927d67305ae334a6c
markkampe <mark.kampe@gmail.com>
2019-12-03 09:52:59 -0800

    check for error output

--- Timing-Sort/test_suite/autograde.sh
+++ Timing-Sort/test_suite/autograde.sh
@@ -1,6 +1,16 @@
 echo "Running SortTimer.main"
+rm -f ERRORS
 java sortCompare.SortTimer
 ret=$?
 echo "==================================="
 echo "test suite exit code: $ret"
-exit $ret
+
+if [ -s ../ERRORS ]
+then
+	echo "==================================="
+	echo "TEST SUITE PRODUCED ERROR OUTPUT"
+	echo
+	exit 1
+else
+	exit $ret
+fi
