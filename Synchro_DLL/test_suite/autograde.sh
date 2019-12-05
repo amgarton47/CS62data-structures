b3a3d432e1d7ce1feda8d9d045798724b078ee03
Mark.Kampe <mark.kampe@gmail.com>
2019-12-05 04:45:16 -0800

    check for analysis.txt, add code diffs

--- Synchro_DLL/test_suite/autograde.sh
+++ Synchro_DLL/test_suite/autograde.sh
@@ -1,6 +1,30 @@
-echo "Running Tester.main"
+# confirm we can find the analysis
+found=`find ../.. -name '*.txt'`
+if [ $? -eq 0 -a -n "$found" ]
+then
+	ls -l $found
+else
+	echo
+	echo "UNABLE TO FIND analysis.txt"
+	echo
+fi
+
+# if we have copies of the starter, diff the changes
+if [ -n "$STARTER" -a -d "$STARTER" ]
+then
+	for f in DLL_Node.java Tester.java
+	do
+		echo "Changes to $f ..."
+		diff -b $STARTER/$f linkedlist/$f
+		echo
+	done
+fi
+
+# run the program to see if race conditions have been eliminated
+echo "... Running Tester.main"
 java linkedlist.Tester --races=ir
 ret=$?
 echo "==================================="
 echo "test suite exit code: $ret"
+
 exit $ret
