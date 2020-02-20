4f8fa99fd8a7613b9f03aa6368dbdb69e3601165
Mark.Kampe <mark.kampe@gmail.com>
2020-02-19 16:44:23 -0800

    more robust about layers in project repo

--- JUnit-ArrayList/test_suite/autograde.sh
+++ JUnit-ArrayList/test_suite/autograde.sh
@@ -1,7 +1,18 @@
 RUNNER="org.junit.platform.console.ConsoleLauncher"
 TESTCASE=ArrayListTest
  
-java -cp '../../../dependencies/*' $RUNNER -c $TESTCASE
+if [ -d ../../../dependencies ]
+then
+	CLASSPATH='.:../../../dependencies/*'
+elif [ -d ../../dependencies ]
+then
+	CLASSPATH='.:../../dependencies/*'
+else
+	echo " ... ERROR: uanble to find dependencies"
+	exit -1
+fi
+
+java -cp $CLASSPATH $RUNNER -c $TESTCASE
 if [ $? -eq 0 ]
 then
 	echo " ... SUCCESSFUL"
