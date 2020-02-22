22534d7d62f10298f791cc1acc3ee1b56e70ac57
Mark Kampe <mark.kampe@gmail.com>
2020-02-21 16:22:01 -0800

    use the pre-established CLASSPATH rather than recreating it

--- JUnit-ArrayList/test_suite/autograde.sh
+++ JUnit-ArrayList/test_suite/autograde.sh
@@ -1,18 +1,9 @@
 RUNNER="org.junit.platform.console.ConsoleLauncher"
 TESTCASE=ArrayListTest
- 
-if [ -d ../../../dependencies ]
-then
-	CLASSPATH='.:../../../dependencies/*'
-elif [ -d ../../dependencies ]
-then
-	CLASSPATH='.:../../dependencies/*'
-else
-	echo " ... ERROR: uanble to find dependencies"
-	exit -1
-fi
 
-java -cp $CLASSPATH $RUNNER -c $TESTCASE
+# CLASSPATH has been set by compile_and_run.sh
+#   but we must add the ArrayListTest class in this directory
+java -cp .:$CLASSPATH $RUNNER -c $TESTCASE
 if [ $? -eq 0 ]
 then
 	echo " ... SUCCESSFUL"
