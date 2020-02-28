7850c545545b9534fe19a53e58d886c301b9fca7
Mark.Kampe <mark.kampe@gmail.com>
2020-02-28 10:06:55 -0800

    update autograde.sh to know about the package

--- Timing-Sort/test_suite/autograde.sh
+++ Timing-Sort/test_suite/autograde.sh
@@ -1,16 +1,6 @@
 echo "Running SortTimer.main"
-rm -f ERRORS
-java sortCompare.SortTimer
+java -cp . sortCompare.SortTimer
 ret=$?
 echo "==================================="
 echo "test suite exit code: $ret"
-
-if [ -s ../ERRORS ]
-then
-	echo "==================================="
-	echo "TEST SUITE PRODUCED ERROR OUTPUT"
-	echo
-	exit 1
-else
-	exit $ret
-fi
+exit $ret
